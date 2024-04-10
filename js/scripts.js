a282dc41633483da085fb7ea37908678b740164a
shareifjones <shareifjones@gmail.com>
2024-04-10 19:40:12 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+alert('Hello World');
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood)
+
+let myName = 'Bob';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
