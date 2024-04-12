3435a5477e3eed5432c88a33a30341d415801504
shareifjones <shareifjones@gmail.com>
2024-04-12 18:51:35 +0200

    Included a loop and specified a big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 let pokemonList = [
-    {name: "Charizard",
+{
+    name: "Charizard",
     height: 1.7,
-    types: ["fire", "flying"]},
-    {name: "Mewtwo",
+    types: ["fire", "flying"]
+},
+{
+    name: "Mewtwo",
     height: 2,
-    types: "psychic"},
-    {name: "Pikachu",
+    types: "psychic"
+},
+{
+    name: "Pikachu",
     height: 0.4,
-    types: "electric"}
+    types: "electric"
+}
 ];
 
+for (let i = 0; i < pokemonList.length; i++){
+document.write (pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ") " )
+    if (pokemonList[i].height >= 2)
+    document.write ("-Wow, thats big! ")
+}
