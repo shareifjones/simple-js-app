d0a345f96ce002c7f4cf67f901814ee1a1ebf0b6
shareifjones <shareifjones@gmail.com>
2024-04-17 21:25:51 +0200

    Tried to include pokemon list/buttons but unsuccessful

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,58 @@
-let pokemonList = [
+var pokemonRepository = (function () {
+  let repository = [
 {
     name: "Charizard",
     height: 1.7,
-    types: ["fire", "flying"]
+    types: ["fire", "flying"],
 },
 {
     name: "Mewtwo",
     height: 2,
-    types: "psychic"
+    types: ["psychic"],
 },
 {
     name: "Pikachu",
     height: 0.4,
-    types: "electric"
-}
+    types: ["electric"],
+  },
 ];
-
-pokemonList.forEach(function(pokemon) {
-    document.write ("<p>" + pokemon.name + " " + "(height: " + pokemon.height + ")");
-    if (pokemon.height >= 2)
-    document.write (" Wow, thats big!" + "</p>")
-});
-
-let pokemonRepository = (function () {
-    let pokemonList = [];
   
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
+function add(pokemon) {
+  if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "height" in pokemon &&
+    "types" in pokemon
+  ) {
+    repository.push(pokemon);
+  } else {
+    console.log("pokemon is not correct");
+  }
+}
     function getAll() {
-      return pokemonList;
+      return repository;
     }
-  
+
+    function addListItem(pokemon){
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+    }
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
+      addListItem: addListItem
     };
   })();
 
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
+pokemonRepository.add({ name: "Charizard", height: 1.7, types: ["fire", "flying"] });
 
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
