c5d31406d1d72756d5b5bf8f1cd8d0f57dc7ea8f
shareifjones <shareifjones@gmail.com>
2024-04-20 17:28:48 +0200

    Added polyfills and updated pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 var pokemonRepository = (function () {
-  let repository = [
-{
-    name: "Charizard",
-    height: 1.7,
-    types: ["fire", "flying"],
-},
-{
-    name: "Mewtwo",
-    height: 2,
-    types: ["psychic"],
-},
-{
-    name: "Pikachu",
-    height: 0.4,
-    types: ["electric"],
-  },
-];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
 function add(pokemon) {
   if (
@@ -24,13 +9,13 @@ function add(pokemon) {
     "height" in pokemon &&
     "types" in pokemon
   ) {
-    repository.push(pokemon);
+    pokemonList.push(pokemon);
   } else {
     console.log("pokemon is not correct");
   }
 }
     function getAll() {
-      return repository;
+      return pokemonList;
     }
 
     function showDetails(pokemon){
@@ -45,27 +30,59 @@ function add(pokemon) {
       button.classList.add("button-class");
       listpokemon.appendChild(button);
       pokemonList.appendChild(listpokemon);
-
-      addButtonListener(button, pokemon);
+      button.addEventListener("click", function(event) {
+        showDetails(pokemon);
+      });
     }
 
-    function addButtonListener(button, pokemon) {
-    button.addEventListener("click", function(){
-      showDetails(pokemon);
+    function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+    function loadDetails(item) {
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
     });
   }
 
     return {
       add: add,
       getAll: getAll,
-      addListItem: addListItem
+      addListItem: addListItem,
+       loadList: loadList,
+      loadDetails: loadDetails,
+      showDetails: showDetails
     };
   })();
 
-pokemonRepository.add({ name: "Onyx", height: 8.8, types: ["rock", "ground"] });
-
-console.log(pokemonRepository.getAll());
-
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
+  });
 });
